f7b50b2335b59d0da5f90dafdeac12c9181d69fe
MJ Cariso <mjcariso@gmail.com>
2023-01-13 22:29:26 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import {useEffect,useState} from 'react';
+import './App.css';
+
+import { MdDarkMode} from "react-icons/md";
+import { HiSun} from "react-icons/hi";
+function App() {
+  const [darkmode,setdarkmode]=useState(false);
+
+  useEffect(()=>{
+
+    if(darkmode){
+      document.body.classList.add("darkmode");
+    }
+    else{
+      document.body.classList.remove("darkmode");
+    }
+  },[darkmode])
+  return (
+    <div className="Container">
+      
+      <label className='switch'>
+        <input type="checkbox" onChange={()=>setdarkmode(!darkmode)} checked={darkmode}/>
+        <span className='slider round'></span>
+        <MdDarkMode className={`fas fa-moon ${darkmode ? 'active' : ''}`} size={25}/>
+     <HiSun className={`fas fa-sun ${darkmode ? '' : 'active'}`} size={25}/>
+      </label>
+</div>
+      
+
+  );
+}
+
+export default App;
