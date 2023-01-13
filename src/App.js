5dd8d3919cd904edd66f07291d3dceb358800e88
MJ Cariso <mjcariso@gmail.com>
2023-01-13 22:46:09 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
+import { useEffect, useState } from "react";
+import "./App.css";
 
-import {useEffect,useState} from 'react';
-import './App.css';
-
-import { MdDarkMode} from "react-icons/md";
-import { HiSun} from "react-icons/hi";
+import { MdDarkMode } from "react-icons/md";
+import { HiSun } from "react-icons/hi";
 function App() {
-  const [darkmode,setdarkmode]=useState(false);
+  const [darkmode, setdarkmode] = useState(false);
 
-  useEffect(()=>{
-
-    if(darkmode){
+  useEffect(() => {
+    if (darkmode) {
       document.body.classList.add("darkmode");
-    }
-    else{
+    } else {
       document.body.classList.remove("darkmode");
     }
-  },[darkmode])
+  }, [darkmode]);
   return (
     <div className="Container">
-      
-      <label className='switch'>
-        <input type="checkbox" onChange={()=>setdarkmode(!darkmode)} checked={darkmode}/>
-        <span className='slider round'></span>
-        <MdDarkMode className={`fas fa-moon ${darkmode ? 'active' : ''}`} size={25}/>
-     <HiSun className={`fas fa-sun ${darkmode ? '' : 'active'}`} size={25}/>
+      <label className="switch">
+        <input
+          type="checkbox"
+          onChange={() => setdarkmode(!darkmode)}
+          checked={darkmode}
+        />
+        <span className="slider round"></span>
+        <MdDarkMode
+          className={`fas fa-moon ${darkmode ? "active" : ""}`}
+          size={25}
+        />
+        <HiSun className={`fas fa-sun ${darkmode ? "" : "active"}`} size={25} />
       </label>
-</div>
-      
-
+    </div>
   );
 }
 
